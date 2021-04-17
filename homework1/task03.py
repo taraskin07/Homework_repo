20a85d94eddf4b31345b16daa91f2b9a2514e520
taraskin07 <82053726+taraskin07@users.noreply.github.com>
2021-04-17 12:47:33 +0300

    Add files via upload

---
+++ homework1/task03.py
@@ -0,0 +1,32 @@
+"""
+Write down the function, which reads input line-by-line, and find maximum and minimum values.
+Function should return a tuple with the max and min values.
+
+For example for [1, 2, 3, 4, 5], function should return [1, 5]
+
+We guarantee, that file exists and contains line-delimited integers.
+
+To read file line-by-line you can use this snippet:
+
+with open("some_file.txt") as fi:
+    for line in fi:
+        ...
+
+"""
+from typing import Tuple
+
+
+def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
+    massive = []
+    for line in file_name:
+        numbers = line.strip().split()
+        for one_number in numbers:
+            massive.append(int(one_number))
+    t = min(massive), max(massive)
+    return t
+
+
+with open("task03.txt", "r") as fi:
+    print(find_maximum_and_minimum(fi))
+
+fi.close()
