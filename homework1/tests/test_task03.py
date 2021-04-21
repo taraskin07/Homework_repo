185e93130750c9b3ab86c486b8fd6284cb0e25b2
taraskin07 <82053726+taraskin07@users.noreply.github.com>
2021-04-21 23:50:39 +0300

    Add files via upload

---
+++ homework1/tests/test_task03.py
@@ -0,0 +1,10 @@
+from tasks.task03 import find_maximum_and_minimum
+
+with open("task03.txt", "r") as fi:
+
+    def min_and_max_in_file():
+        """Testing that function return minimum -15 and maximum 566 from *.txt file"""
+        assert find_maximum_and_minimum(fi) == (-15, 566)
+
+
+fi.close()
---
+++ homework1/tests/test_task05.py
@@ -0,0 +1,36 @@
+from tasks.task05 import find_maximal_subarray_sum
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+
+def example_check():
+    """Testing that example list gives 16"""
+    assert find_maximal_subarray_sum(nums, k) == 16
+
+
+nums = []
+k = 3
+
+
+def example_check_empty():
+    """Testing that empty list gives 0"""
+    assert find_maximal_subarray_sum(nums, k) == 0
+
+
+nums = [0, 0, 0, 0]
+k = 3
+
+
+def example_check_zero():
+    """Testing that zero list gives 0"""
+    assert find_maximal_subarray_sum(nums, k) == 0
+
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 1
+
+
+def example_check_maximum():
+    """Testing that k=1 returns maximum"""
+    assert find_maximal_subarray_sum(nums, k) == max(nums)
