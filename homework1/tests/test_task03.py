09e5fc4d064fc5898a7eed6bb46eef9ec3c54be8
taraskin07 <taraskin_07@bk.ru>
2021-04-24 00:51:45 +0300

    fixed_everything

--- homework1/tests/test_task03.py
+++ homework1/tests/test_task03.py
@@ -1,10 +1,13 @@
+import os
+
 from homework1.tasks.task03 import find_maximum_and_minimum
 
 
 def test_min_and_max_in_file():
     """Testing that function return minimum -15 and maximum 566 from *.txt file"""
+    abs_path = os.getcwd() + "\\homework1\\tests\\task03.txt"
     with open(
-        "C:\\Users\\AngryKid\\Repositories\\Homework_repo\\homework1\\tests\\task03.txt",
+        abs_path,
         "r",
     ) as fi:
         assert find_maximum_and_minimum(fi) == (-15, 566)
