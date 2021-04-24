b2b63fbf68ff920fd2c03d11f0be8b5ced0ff638
taraskin07 <taraskin_07@bk.ru>
2021-04-24 15:55:24 +0300

    paths have been changed

--- homework2/tests/test_hw1.py
+++ homework2/tests/test_hw1.py
@@ -6,7 +6,8 @@ from homework2.tasks.hw1 import *
 
 def test_get_longest_diverse_words():
     """Testing that 10 last words are the longest and in ordered fashion instead of random"""
-    abs_path = os.getcwd() + "\\homework2\\tests\\data.txt"
+    path = os.path.dirname(os.path.abspath(__file__))
+    abs_path = os.path.join(path, "data.txt")
     with codecs.open(
         abs_path,
         "r",
@@ -19,7 +20,8 @@ def test_get_longest_diverse_words():
 
 def test_get_rarest_char():
     """Testing that rarest char should appear only 1 time"""
-    abs_path = os.getcwd() + "\\homework2\\tests\\data.txt"
+    path = os.path.dirname(os.path.abspath(__file__))
+    abs_path = os.path.join(path, "data.txt")
     with codecs.open(
         abs_path,
         "r",
@@ -30,7 +32,8 @@ def test_get_rarest_char():
 
 def test_count_punctuation_chars():
     """Testing that there are 4213 punctuation chars"""
-    abs_path = os.getcwd() + "\\homework2\\tests\\data.txt"
+    path = os.path.dirname(os.path.abspath(__file__))
+    abs_path = os.path.join(path, "data.txt")
     with codecs.open(
         abs_path,
         "r",
@@ -41,7 +44,8 @@ def test_count_punctuation_chars():
 
 def test_get_most_common_non_ascii_char():
     """Testing that the most common non ascii char is not ascii"""
-    abs_path = os.getcwd() + "\\homework2\\tests\\data.txt"
+    path = os.path.dirname(os.path.abspath(__file__))
+    abs_path = os.path.join(path, "data.txt")
     with codecs.open(
         abs_path,
         "r",
