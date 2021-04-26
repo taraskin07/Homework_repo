9f51fb4966d64715b5c0ca2aea6d071d5e7fd4c0
taraskin07 <taraskin_07@bk.ru>
2021-04-26 22:06:52 +0300

    fixes

--- homework3/tests/test_task01.py
+++ homework3/tests/test_task01.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from homework3.tasks.task01 import *
 
 
@@ -6,22 +8,36 @@ def func(a, b):
     return (a ** b) ** 2
 
 
-def test_uncached_values():
-    """Testing that only 3 values are cached"""
-    some = 100, 200
+# def test_uncached_values():
+#     """Testing that only 3 values are cached"""
+#     some = 100, 200
+#
+#     val_1 = func(*some)
+#     val_2 = func(*some)
+#     val_3 = func(*some)
+#     val_4 = func(*some)
+#     assert not val_3 is val_4
+#
+#
+# def test_cached_values():
+#     """Testing that 3 values are cached"""
+#     some = 100, 200
+#
+#     val_1 = func(*some)
+#     val_2 = func(*some)
+#     val_3 = func(*some)
+#     assert val_3 is val_2 and val_2 is val_1
 
+
+def test_calls_of_func():
+    func = Mock()
+    some = 100, 200
     val_1 = func(*some)
+    func.assert_called()
+    func.assert_called_once()
     val_2 = func(*some)
+    func.assert_called()
     val_3 = func(*some)
+    func.assert_called()
     val_4 = func(*some)
-    assert not val_3 is val_4
-
-
-def test_cached_values():
-    """Testing that 3 values are cached"""
-    some = 100, 200
-
-    val_1 = func(*some)
-    val_2 = func(*some)
-    val_3 = func(*some)
-    assert val_3 is val_2 and val_2 is val_1
+    func.assert_called()
