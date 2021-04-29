193ac7e7e5556695e4644aeadfa4e7d68d24c7d7
taraskin07 <taraskin_07@bk.ru>
2021-04-29 17:39:22 +0300

    homework4

--- homework3/tasks/task01.py
+++ homework3/tasks/task01.py
@@ -21,51 +21,41 @@ assert val_1 is val_2
 from typing import Callable
 
 
-def func(a, b):  # Сама функция, которую надо задекорировать
+def func(a, b):
     return (a ** b) ** 2
 
 
 def new_cache(
     times,
-):  # Параметризованный декоратор, где параметр times говорит сколько раз нужно закэшировать значение, если times=3, значит 3 раза, не учитывая самый первый вызов, когда значение кэшировалось
+):
     counter = 0
     cache_value = dict()
 
     def cache_func(
         func: Callable,
-    ) -> Callable:  # функция принимающая на вход искомую функцию func, объявленную раньше
-        def the_wrapper(
-            a, b
-        ):  # функция принимающая на вход значения аргументов функции func, объявленной ранее
+    ) -> Callable:
+        def the_wrapper(a, b):
             nonlocal counter, cache_value
             new_value = (
                 a,
                 b,
-            )  # кортеж с аргументами искомой функции func, объявленной ранее, так просто, для удобства восприятия
-            if (
-                counter >= times
-            ):  # проверяем, не обратились ли мы к кэшу более 3х раз, по умолчанию counter == 1
-                cache_value.clear()  # если же это уже 4-е обращение, очищаем кэш
-                counter = 0  # если же это уже 4-е обращение, сбрасываем счетчик обращений на исходную counter == 1
-            if (
-                new_value in cache_value
-            ):  # а вот если это уже 2-е или 3-е обращение (up to 'times' value)
-                counter += 1  # увеличиваем счетчик на 1
-                return cache_value[new_value]  # возвращаем результат из словаря
-            cache_value[new_value] = func(
-                a, b
-            )  # в противном случае, записываем результат в словарь
-            calculation = cache_value[
-                new_value
-            ]  # для красоты обзываем этот результат новой переменной, которую затем и возвращаем
-            return calculation  # возвращаем тут
+            )
+            if counter >= times:
+                cache_value.clear()
+                counter = 0
+            if new_value in cache_value:
+                counter += 1
+                return cache_value[new_value]
+            cache_value[new_value] = func(a, b)
+            calculation = cache_value[new_value]
+            return calculation
 
         return the_wrapper
 
     return cache_func
 
 
-@new_cache(times=3)  # декорируем эту несчастную функцию
+@new_cache(times=3)
 def func(a, b):
     return (a ** b) ** 2
 
@@ -83,9 +73,7 @@ if __name__ == "__main__":
     print(val_3)
     print(val_4)
     print(val_5)
-    print(val_1 is val_2)  # кэшируются ли 1й и 2й вызов функции, True
-    print(val_2 is val_3)  # кэшируются ли 2й и 3й вызов функции, True
-    print(val_3 is val_4)  # кэшируются ли 3й и 4й вызов функции, True
-    print(
-        val_4 is val_5
-    )  # кэшируются ли 4й и 5й вызов функции, False, 5й уже не из кэша, считается заново
+    print(val_1 is val_2)
+    print(val_2 is val_3)
+    print(val_3 is val_4)
+    print(val_4 is val_5)
---
+++ homework4/tests/test_task_3_get_print_output.py
@@ -0,0 +1,44 @@
+"""
+Write a function that will receive a string and write it to stderr
+if line starts with "error" and to the stdout otherwise.
+
+
+my_precious_logger("error: file not found")
+# stderr
+'error: file not found'
+
+
+my_precious_logger("OK")
+# stdout
+'OK'
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has positive tests
+
+You will learn:
+ - how to write to stderr
+ - how to test output to the stderr and stdout
+"""
+import sys
+
+
+def my_precious_logger(text: str):
+    if "error" in text:
+        print(text, file=sys.stderr)
+    else:
+        print(text)
+
+
+def test_my_precious_logger_error(capsys):
+    my_precious_logger("error: file not found")
+
+    captured = capsys.readouterr()
+    assert captured.err == "error: file not found\n"
+
+
+def test_my_precious_logger_output(capsys):
+    my_precious_logger("OK")
+    captured = capsys.readouterr()
+    assert captured.out == "OK\n"
