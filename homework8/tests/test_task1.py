47fc7333fedad85db51f5d0c63d5a139a23add16
taraskin07 <82053726+taraskin07@users.noreply.github.com>
2021-06-15 17:08:09 +0300

    Add files via upload

---
+++ homework8/tests/test_task1.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+
+from homework8.tasks.task1 import KeyValueStorage
+
+path = os.path.dirname(os.path.abspath(__file__))
+abs_path = os.path.join(path, "test_task1.txt")
+
+
+def test_text_contents():
+    path = os.path.dirname(os.path.abspath(__file__))
+    abs_path = os.path.join(path, "test_task1.txt")
+    storage = KeyValueStorage(abs_path)
+    assert storage["name"] == "kek"
+    assert storage.song_name == "shadilay"
+    assert storage.power == 9001
+    assert storage.last_name == "top"
+    assert storage["dictionary"] == "test"
+
+
+def test_error_raised(tmpdir):
+    p = tmpdir.join("something.txt")
+    p.write("1=something")
+    with pytest.raises(ValueError):
+        storage = KeyValueStorage(p)
+        assert storage["1"]
+        assert storage[1]
---
+++ homework8/tests/test_task2_context_manager.py
@@ -0,0 +1,47 @@
+import os
+
+from homework8.tasks.task2_context_manager import TableData
+
+path = os.path.dirname(os.path.abspath(__file__))
+abs_path = os.path.join(path, "example.sqlite")
+
+
+def test_length_assertion_and_contain_presidents():
+    with TableData(database_name=abs_path, table_name="presidents") as presidents:
+        assert len(presidents) == 3
+        assert "Yeltsin" in presidents
+        assert "Trump" in presidents
+        assert "Big Man Tyrone" in presidents
+        assert not "Obama" in presidents
+
+
+def test_iteration_presidents():
+    with TableData(database_name=abs_path, table_name="presidents") as presidents:
+        new_list = []
+        for president in presidents:
+            new_list.append(president["name"])
+        assert new_list == ["Yeltsin", "Trump", "Big Man Tyrone"]
+
+
+def test_content_presidents():
+    with TableData(database_name=abs_path, table_name="presidents") as presidents:
+        assert presidents["Yeltsin"] == ("Yeltsin", 999, "Russia")
+
+
+def test_length_assertion_and_contains_books():
+    with TableData(database_name=abs_path, table_name="books") as books:
+        assert len(books) == 3
+        assert "1984" in books
+
+
+def test_iteration_books():
+    with TableData(database_name=abs_path, table_name="books") as books:
+        new_list2 = []
+        for book in books:
+            new_list2.append(book["name"])
+        assert new_list2 == ["Farenheit 451", "Brave New World", "1984"]
+
+
+def test_content_books():
+    with TableData(database_name=abs_path, table_name="books") as books:
+        assert books["Farenheit 451"] == ("Farenheit 451", "Bradbury")
